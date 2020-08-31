800b5304d25dea04748d2664ab37c6048ae43298
ArgAnkit1994 <68583118+ArgAnkit1994@users.noreply.github.com>
2020-09-01 05:25:05 +0530

    Updates

---
+++ precache-manifest.60b703afd6f1897687be38989307c4b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff50cb75e51ae8369908c44a91dd7ccc",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "0f6336f6614796d7a05d",
+    "url": "/weather-forecast/static/js/2.1f85d0f5.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/weather-forecast/static/js/2.1f85d0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f918d9c807be53a105a",
+    "url": "/weather-forecast/static/js/main.a448940b.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  }
+]);
